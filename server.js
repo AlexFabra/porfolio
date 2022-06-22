238128c4773bddd72f22eeddfe323a4d7e9b79da
Alejandro <alfaseart@gmail.com>
2022-06-23 00:03:34 +0200

    changes on index

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 // Serve static files
 app.use(express.static('/dist/AlejandroFabraPortfolio'));
@@ -10,5 +11,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join( __dirname,'/dist/AlejandroFabraPortfolio/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT, ()=>{
+  console.log(`servidor corriendo en el puerto ${process.env.PORT}`)
+})
