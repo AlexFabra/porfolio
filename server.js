5bea3244be4573ad866c735cf5ef9b06a2715556
Alejandro <alfaseart@gmail.com>
2022-06-22 23:20:46 +0200

    second heroku deploy

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/AlejandroFabraPortfolio'));
+app.use(express.static('/dist/AlejandroFabraPortfolio'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/AlejandroFabraPortfolio/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join( __dirname,'/dist/AlejandroFabraPortfolio/index.html'));
 });
 
 // default Heroku port
