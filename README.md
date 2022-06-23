b60c7812e49b873def3b8133ca5888781539eb4c
Alejandro <alfaseart@gmail.com>
2022-06-23 10:38:52 +0200

    production

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AlejandroFabraPortfolio
+# alejandro-fabra-portfolio
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/AlejandroFabraPortfolio'),
+      dir: require('path').join(__dirname, './coverage/alejandro-fabra-portfolio'),
       subdir: '.',
       reporters: [
         { type: 'html' },
