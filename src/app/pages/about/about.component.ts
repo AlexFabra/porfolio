82af155fe26f13e90ba4cd3d461bfc654d000641
Alejandro <alfaseart@gmail.com>
2022-07-31 17:20:18 +0200

    new certificate

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  languages:any[] = [{name:'Anglès',img:'https://res.cloudinary.com/dlmdj7zsu/image/upload/v1655978067/english_ymrs4j.png',level:'B1'},{name:'Català',img:'https://res.cloudinary.com/dlmdj7zsu/image/upload/v1655978067/cat_r3pd3g.png',level:'C1'},{name:'Castellà',img:'https://res.cloudinary.com/dlmdj7zsu/image/upload/v1655978066/esp_xwvoa1.png',level:'Nadiu'}]
+  languages:any[] = [{name:'Anglès',img:'https://res.cloudinary.com/dlmdj7zsu/image/upload/v1655978067/english_ymrs4j.png',level:'B1'},{name:'Català',img:'https://res.cloudinary.com/dlmdj7zsu/image/upload/v1655978067/cat_r3pd3g.png',level:'Nadiu'},{name:'Castellà',img:'https://res.cloudinary.com/dlmdj7zsu/image/upload/v1655978066/esp_xwvoa1.png',level:'Nadiu'}]
 
   constructor() { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:'https://alejandro-fabra-portfolio.herokuapp.com'
+  baseUrl:'https://alejandro-fabra-portfolio.herokuapp.com/portfolio'
 };
